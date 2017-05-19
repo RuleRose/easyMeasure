9e24fcf965afcfb9e18c8ef46afcfac0ab42ccdc
yongche <xiaojun@yongche.com>
2017-05-19 09:53:04 +0800

    Initial Commit

---
+++ easyMeasure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  easyMeasure
+//
+//  Created by yongche on 17/5/19.
+//  Copyright © 2017年 goldsmith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
