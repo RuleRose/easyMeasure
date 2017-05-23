24fad6950a4162435efc88356b80bf9014f317d9
yongche <xiaojun@yongche.com>
2017-05-23 10:49:14 +0800

    localization

---
+++ easyMeasure/initServer.h
@@ -0,0 +1,13 @@
+//
+//  initServer.h
+//  easyMeasure
+//
+//  Created by yongche on 17/5/23.
+//  Copyright © 2017年 goldsmith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface initServer : NSObject
+
+@end
